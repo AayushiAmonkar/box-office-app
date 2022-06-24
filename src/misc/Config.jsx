4393a3528865bdcaf529a802074d7fb48e61c2ea
AayushiAmonkar <aayushiamonkar@gmail.com>
2022-06-24 12:50:54 +0530

    created home page

---
+++ src/misc/Config.jsx
@@ -0,0 +1,17 @@
+const API_BASE_URL = "https://api.tvmaze.com";
+
+const apiGet = async (queryString) => {
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then((r) =>
+    r.json()
+  );
+  return response;
+
+  // fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+  // .then((r) => r.json())
+  // .then((res) => {
+  //   setResult(res);
+  //   console.log(res);
+  // });
+};
+
+export { apiGet };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import MainPageLayout from "../components/MainPageLayout";
-
+import { apiGet } from "../misc/Config";
 const Home = () => {
   const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+  const [searchOption, setSearchOption] = useState("shows");
+
+  const isShowsSearch = searchOption === "shows";
   const onInputChange = (event) => {
     // console.log(event.target.value);
     setInput(event.target.value);
   };
   const onSearch = () => {
     // https://api.tvmaze.com/search/shows?q=girls
-
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then((r) => r.json())
-      .then((res) => console.log(res));
+    apiGet(`/search/${searchOption}?q=${input}`).then((res) => {
+      setResult(res);
+      // console.log(res);
+    });
   };
   const onKeyDown = (event) => {
     // console.log(event.keyCode);
@@ -20,17 +24,62 @@ const Home = () => {
       onSearch();
     }
   };
+  const renderResult = () => {
+    // console.log(result);
+    if (result && result.length === 0) {
+      //no such movie
+      return <div>No results</div>;
+    }
+    if (result && result.length > 0) {
+      return result[0].show
+        ? result.map((item) => <div key={item.show.id}>{item.show.name}</div>)
+        : result.map((item) => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
+    }
+    return null;
+  };
+
+  const onRadioChange = (event) => {
+    setSearchOption(event.target.value);
+  };
   return (
     <MainPageLayout>
       <input
         type="text"
+        placeholder="Search For Something"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
         value={input}
       />
+      <div>
+        <label htmlFor="shows-search">
+          Shows
+          <input
+            type="radio"
+            name=""
+            id="shows-search"
+            value="shows"
+            checked={isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="actor-search">
+          Actor
+          <input
+            type="radio"
+            name=""
+            id="actor-search"
+            value="people"
+            checked={!isShowsSearch}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResult()}
     </MainPageLayout>
   );
 };
